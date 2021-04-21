ebe2509441ab91a669b23187a45272365babd6d4
chan9279 <moonji9279@gmail.com>
2021-04-21 19:27:55 +0900

    초기 커밋.

---
+++ 20210421/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _20210421
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputNumber = int.Parse(input);
+            for(int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(inputNumber + "X" + i + "=" + (inputNumber * i));
+            }
+        }
+    }
+}
